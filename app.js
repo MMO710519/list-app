693653f40755ef674e719809f02ec8c3d75d98c9
MO7105 <muu_goworld.1006@hotmail.co.jp>
2020-10-22 16:25:16 +0900

    Add index page

--- app.js
+++ app.js
@@ -13,5 +13,9 @@ app.get('/top', (req, res) => {
     res.render('top.ejs');
 })
 
+//一覧画面を表示するルーティング
+app.get('/index', (req, res) => {
+    res.render('index.ejs');
+})
 //サーバーを起動するコード
 app.listen(3000);
