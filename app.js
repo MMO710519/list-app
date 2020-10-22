f932549d6875998f7fd953adbf955743b3f26e1e
MO7105 <muu_goworld.1006@hotmail.co.jp>
2020-10-22 15:53:44 +0900

    Add top page

--- app.js
+++ app.js
@@ -5,4 +5,10 @@ app.get('/', (req, res) => {
     res.render('hello.ejs');
 });
 
+//トップ画面を表示するルーティング
+app.get('/top', (req, res) => {
+    res.render('top.ejs');
+})
+
+//サーバーを起動するコード
 app.listen(3000);
