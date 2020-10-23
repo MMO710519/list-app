0cba1551428789243ea7a392c0f62933343c4476
MO7105 <muu_goworld.1006@hotmail.co.jp>
2020-10-23 14:43:58 +0900

    Add database info

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+//MySQLを使うためのコード
+const mysql = require('mysql');
+
 const app = express();
 
 //cssや画像ファイルを置くフォルダを指定する
 app.use(express.static('public'));
+// DBへの接続情報
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'mysqlmysql',
+    database: 'list_app'
+});
 
 app.get('/', (req, res) => {
     res.render('top.ejs');
