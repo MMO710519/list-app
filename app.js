9ba57bc08bede7d9d2bb39f2887552410faa54f3
MO7105 <muu_goworld.1006@hotmail.co.jp>
2020-10-26 15:40:28 +0900

    Add edit function

--- app.js
+++ app.js
@@ -56,5 +56,25 @@ app.post('/delete/:id',(req,res)=>{
     )
 });
 
+app.get('/edit/:id',(req,res)=>{
+    connection.query(
+        'SELECT name FROM items WHERE id=?',
+        [req.params.id],
+        (error,results)=>{
+            res.render('edit.ejs', {item:results[0]});
+        }
+    );
+});
+
+app.post('/update/:id', (req,res)=>{
+    connection.query(
+        'UPDATE items SET name=? WHERE id=?',
+        [req.body.itemName,req.params.id],
+        (error,results)=>{
+            res.redirect('/index');
+        }
+    );
+});
+
 //サーバーを起動するコード
 app.listen(3000);
